4cc11ebf0c55d9f66839e530783b20df936558ba
dev-marJ <152494760+dev-marJ@users.noreply.github.com>
2023-12-09 18:11:47 +0200

    edited js

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ const testimonials = [
     {
         name: "Tanya Sinclair",
         job: "UX Engineer",
-        image: "/images/image-tanya.jpg",
+        image: "./images/image-tanya.jpg",
         testimonial: " I've been interested in coding for a while <br> but never taken the jump, until now. I <br> couldn't recommend this course enough. <br> I'm now in the job of my dreams and so <br> exicted about the future. ", 
     },
     {
         name: "John Tarkpor",
         job: "Junior Front-end Developer",
-        image: "/images/image-john.jpg",
+        image: "./images/image-john.jpg",
         testimonial: " If you want to lay the best foundation <br> possible I'd recommend taking this <br> course. The depth the instructors go into <br> is incredible. I now feel so confident about <br> starting up as a professional developer. ", 
     }
 ]
