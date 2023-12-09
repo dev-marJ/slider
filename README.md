4cb5769945a4c6c20d69e1d47fdb177a68286b69
dev-marJ <152494760+dev-marJ@users.noreply.github.com>
2023-12-09 17:57:06 +0200

    finished js section

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# slider
+# Frontend Mentor - Coding bootcamp testimonials slider solution
+
+This is a solution to the [Coding bootcamp testimonials slider challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/coding-bootcamp-testimonials-slider-4FNyLA8JL). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+
+
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the component depending on their device's screen size
+
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- HTML5
+- CSS 
+- Flexbox
+- JavScript
+- Mobile-first workflow
+
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,49 @@
+const leftBtn = document.querySelector(".leftBtn");
+const rightBtn = document.querySelector(".rightBtn");
+let img = document.querySelector(".prof-img");
+let testimonialText = document.querySelector(".testimonial-text");
+let name = document.querySelector(".name");
+let role = document.querySelector(".role");
+
+const testimonials = [
+    {
+        name: "Tanya Sinclair",
+        job: "UX Engineer",
+        image: "/images/image-tanya.jpg",
+        testimonial: " I've been interested in coding for a while <br> but never taken the jump, until now. I <br> couldn't recommend this course enough. <br> I'm now in the job of my dreams and so <br> exicted about the future. ", 
+    },
+    {
+        name: "John Tarkpor",
+        job: "Junior Front-end Developer",
+        image: "/images/image-john.jpg",
+        testimonial: " If you want to lay the best foundation <br> possible I'd recommend taking this <br> course. The depth the instructors go into <br> is incredible. I now feel so confident about <br> starting up as a professional developer. ", 
+    }
+]
+
+
+let counter = 0;
+
+function displayTestimonial(index) {
+    img.src = testimonials[index].image;
+    testimonialText.innerHTML = testimonials[index].testimonial;
+    name.innerHTML = testimonials[index].name;
+    role.innerHTML = testimonials[index].job;
+}
+
+rightBtn.addEventListener("click", () => {
+    countUpToTwo();
+    displayTestimonial(counter);
+});
+
+leftBtn.addEventListener("click", () => {
+    counter = (counter - 1 + testimonials.length) % testimonials.length;
+    displayTestimonial(counter);
+});
+
+function countUpToTwo() {
+    counter = (counter + 1) % testimonials.length;
+    console.log(counter);
+}
+
+
+displayTestimonial(counter);
